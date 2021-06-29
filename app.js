2e357a818fe018604b6152589f80d6b5cb21ecbb
guimariz <guimariz@gmail.com>
2021-06-29 12:38:04 -0300

    Get products created

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Import Routes
+const productsRoute = require('./routes/products');
+const usersRoute = require('./routes/users');
 
-var app = express();
+// Use Routes
+app.use('/api/products', productsRoute);
+app.use('/api/users', usersRoute);
+
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
+    allowedHeaders:
+      'Content-Type, Authorization, Origin, X-Requested-With, Accept',
+  })
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,16 +33,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ routes/products.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const { database } = require('../config/helpers');
+
+/* GET ALL PRODUCTS */
+router.get('/', function (req, res) {
+  let page =
+    req.query.page !== undefined && req.query.page !== 0 ? req.query.page : 1;
+  const limit =
+    req.query.limit !== undefined && req.query.limit !== 0
+      ? req.query.limit
+      : 10;
+
+  let startValue;
+  let endValue;
+
+  if (page > 0) {
+    startValue = page * limit - limit;
+    endValue = page * limit;
+  } else {
+    startValue = 0;
+    endValue = 10;
+  }
+
+  database
+    .table('products as p')
+    .join([
+      {
+        table: 'categories as c',
+        on: 'c.id = p.cat_id',
+      },
+    ])
+    .withFields([
+      'c.title as category',
+      'p.title as name',
+      'p.price',
+      'p.quantity',
+      'p.image',
+      'p.id',
+    ])
+    .slice(startValue, endValue)
+    .sort({ id: 0.1 })
+    .getAll()
+    .then((prods) => {
+      if (prods.length > 0) {
+        res.status(200).json({
+          count: prods.length,
+          products: prods,
+        });
+      } else {
+        res.json({ message: 'No products founds' });
+      }
+    })
+    .catch((err) => console.log(err));
+});
+
+module.exports = router;
